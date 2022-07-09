24dee4abdd95cd57b942dfc869302886416ad123
Rishabh Singh <ris8645208@gmail.com>
2022-07-09 13:28:23 +0530

    Add project files.

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+using System.Data;
+using SqlServerRestApi.Models;
+
+namespace SqlServerRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select * from dbo.Department";
+
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("SqlDataBase");
+            SqlDataReader myReader;
+            using(SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            var jsonStr = JsonConvert.SerializeObject(table);
+            var department = JsonConvert.DeserializeObject<IList<Department>>(jsonStr);
+            return new JsonResult(department);
+
+        }
+
+    }
+}
---
+++ Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace SqlServerRestApi.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DeptName { get; set; }
+    }
+}
